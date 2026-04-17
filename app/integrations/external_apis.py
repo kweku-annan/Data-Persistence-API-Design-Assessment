f551550358e1f99b8e759e529c287971db8f5ffa
kweku-annan <kwekuannan.dev@gmail.com>
2026-04-17 19:04:50 +0000

    fix: fix key error in profile_service

--- app/integrations/external_apis.py
+++ app/integrations/external_apis.py
@@ -103,7 +103,7 @@ async def fetch_profile_data(name: str) -> ExternalProfileData:
 
     return ExternalProfileData(
         gender=gender_data["gender"],
-        gender_probability=gender_data["gender_probability"],
+        gender_probability=gender_data["probability"],
         sample_size=gender_data["count"],
         age=age,
         age_group=_classify_age_group(age),
