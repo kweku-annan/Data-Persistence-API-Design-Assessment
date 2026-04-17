df793013c354803050d27e66ae862c11eda0f883
kweku-annan <kwekuannan.dev@gmail.com>
2026-04-17 12:31:08 +0000

    chore: setup database configuration

--- app/database.py
+++ app/database.py
@@ -1,4 +1,20 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker, Session
+from sqlalchemy import create_engine
+from app.config import settings
 
 
 Base = declarative_base()
+
+engine = create_engine(
+    settings.database_url,
+    connect_args={"check_same_thread": False} # SQLite-specific
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
