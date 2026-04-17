22b16f2dfd7d114aac95cd997f188437d485f933
kweku-annan <kwekuannan.dev@gmail.com>
2026-04-17 13:23:03 +0000

    chore: define pydantic schema for input and output data

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 
 engine = create_engine(
     settings.database_url,
-    connect_args={"check_same_thread": False} # SQLite-specific
+    connect_args={"check_same_thread": False} if "sqlite" in settings.database_url else {} # SQLite-specific
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
---
+++ app/schemas/profile.py
@@ -0,0 +1,46 @@
+from datetime import datetime,
+
+from pydantic import BaseModel, field_validator
+
+
+
+class ProfileCreate(BaseModel):
+    name: str
+
+    @field_validator('name')
+    @classmethod
+    def name_must_not_be_empty(cls, v: str) -> str:
+        if not v or not v.strip():
+            raise ValueError('name must not be empty')
+        if not v.strip().replace(" ", "").replace("-", "").isalpha():
+            raise ValueError('name must be a valid string (only letters, spaces, and hyphens allowed)')
+        return v.strip().lower()
+
+
+class ProfileResponse(BaseModel):
+    id: str
+    name: str
+    gender: str
+    gender_probability: float
+    sample_size: int
+    age: int
+    age_group: str
+    country_id: str
+    country_probability: float
+    created_at: datetime
+
+    model_config = {
+        "from_attributes": True,
+        "json_encoders": {datetime: lambda v: v.strftime("%Y-%m-%dT%H:%M:%SZ")}
+    }
+
+
+class ProfileListItem(BaseModel):
+    id: str
+    name: str
+    gender: str
+    age: int
+    age_group: str
+    country_id: str
+
+    model_config = {"from_attributes": True}
